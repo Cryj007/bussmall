ff0d47618f3290846afb916805fc667fe79b6cc8
CryJ <crystal.jimenez11@gmail.com>
2020-02-18 15:29:34 -0800

    added the pcs

--- app.js
+++ app.js
@@ -15,6 +15,11 @@
 //gloabl variables
 var imageOne= document.getElementById('image1');
 var imageTwo= document.getElementById('image2');
+var imageThree= document.getElementById('image3');
+var imageFour= document.getElementById('image4');
+var imageFive= document.getElementById('image5');
+var imageSix= document.getElementById('image6');
+var imageSeven= document.getElementById('image7');
 var totalClicks= 0
 var allBusImages=[]; //--> array to store random generated images
 var chart= null; //-->i believe it can be changed later, no current value yet
@@ -35,6 +40,11 @@ function busItems(name, imagePath){ //-->the properties I want to show up in my 
 
 new busItems('Bag', './img/bag.jpg'); //--> the thing I want to show up with the properties above
 new busItems('Banana', './img/banana.jpg');
+new busItems('Bathroom', './img/bathroom.jpg');
+new busItems('Bubblegum', './img/bubblegum.jpg');
+new busItems('Cthulhu', './img/Cthulhu.jpg');
+new busItems('Dog-Duck', './img/dog-duck.jpg');
+new busItems('Shark', './img/shark.jpg');
 
 //put images in html
 
@@ -57,16 +67,37 @@ function randomRoll() {
 //render images
 //no duplicates
 
-var one = document.getElementById('image1');
 var randomImage1 = randomRoll();
+var one = document.getElementById('image1');
 var two = document.getElementById('image2');
+var three = document.getElementById('image3');
+var four = document.getElementById('image4');
+var five = document.getElementById('image5');
+var six = document.getElementById('image6');
+var seven = document.getElementById('image7');
 
 
 // console.log('HELLOOO ', randomImage1);
 one.setAttribute('src', randomImage1.image);
 one.setAttribute('alt', randomImage1.name);
+
 two.setAttribute('src', randomImage1.image);
 two.setAttribute('alt', randomImage1.name);
+
+three.setAttribute('src', randomImage1.image);
+three.setAttribute('alt', randomImage1.name);
+
+four.setAttribute('src', randomImage1.image);
+four.setAttribute('alt', randomImage1.name);
+
+five.setAttribute('src', randomImage1.image);
+five.setAttribute('alt', randomImage1.name);
+
+six.setAttribute('src', randomImage1.image);
+six.setAttribute('alt', randomImage1.name);
+
+seven.setAttribute('src', randomImage1.image);
+seven.setAttribute('alt', randomImage1.name);
 
 // function renderImage
   
